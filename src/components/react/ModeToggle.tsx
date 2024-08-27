cfd22cd28813955e7a861f920c8552b4e1a70cae
David Buckley <runalldayandnight@gmail.com>
2024-08-27 08:26:03 -0600

    reorder nav

--- src/components/react/ModeToggle.tsx
+++ src/components/react/ModeToggle.tsx
@@ -9,22 +9,28 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
+export function setDark(theme?: string) {
+  const isDark =
+    !!new URLSearchParams(window.location.search).get("dark") ||
+    theme === "dark" ||
+    (theme === "system" &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches);
+  document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+}
+
 export function ModeToggle() {
   const [theme, setThemeState] = React.useState<
     "theme-light" | "dark" | "system"
   >("theme-light");
 
   React.useEffect(() => {
+    console.log("dark query");
     const isDarkMode = document.documentElement.classList.contains("dark");
     setThemeState(isDarkMode ? "dark" : "theme-light");
   }, []);
 
   React.useEffect(() => {
-    const isDark =
-      theme === "dark" ||
-      (theme === "system" &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches);
-    document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+    setDark(theme);
   }, [theme]);
 
   return (
